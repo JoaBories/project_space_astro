e9bc160c8d3975a54603e5f3e8f356ab69ca51f4
joabories <joa.bories@gmail.com>
2024-09-26 12:02:57 +0200

    begin velocity indicator implementation

--- Assets/scripts/character/VelocitIndicator.cs
+++ Assets/scripts/character/VelocitIndicator.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class VelocitIndicator : MonoBehaviour
 {
-    GameObject player;
+    public GameObject player;
+
+    float scale;
+    float rotation;
+    Vector2 VelocityVector;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,8 @@ public class VelocitIndicator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        VelocityVector = player.GetComponent<Rigidbody2D>().velocity;
+        scale = VelocityVector.magnitude;
+        //rotation = VelocityVector;
     }
 }
